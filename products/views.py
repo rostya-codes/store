ef09e8b6b35a8656b3174ef6011a0cad0472443e
rostya <rostyamudrik@proton.me>
2024-05-10 16:37:11 +0300

    Context added

--- products/views.py
+++ products/views.py
@@ -2,8 +2,36 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+        'is_promotion': True,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': '/static/vendor/img/products/Adidas-hoodie.png',
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Синяя куртка The North Face',
+                'price': 23_725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
+            },
+            {
+                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 3390,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
+            },
+
+        ]
+    }
+    return render(request, 'products/products.html', context)
