6d126a0d6538d04f7a36a72652689400caf06c9b
rostya <rostyamudrik@proton.me>
2024-05-11 11:35:38 +0300

    Models created

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """ ProductCategory model """
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True)  # Может быть пустым
+
+
+class Product(models.Model):
+    """ Product model """
+
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
