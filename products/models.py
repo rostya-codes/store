106aa97dc8b80e97ca41341e2bbb0c5e33b9c006
rostya <rostyamudrik@proton.me>
2024-06-07 10:47:35 +0300

    Some changes but products don't adding in admin panel

--- products/models.py
+++ products/models.py
@@ -7,6 +7,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)  # Может быть пустым
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     """ Product model """
@@ -17,3 +20,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
--- products/views.py
+++ products/views.py
@@ -13,25 +13,48 @@ def products(request):
     context = {
         'title': 'Store - Каталог',
         'products': [
+            # 1
             {
                 'image': '/static/vendor/img/products/Adidas-hoodie.png',
                 'name': 'Худи черного цвета с монограммами adidas Originals',
                 'price': 6090,
                 'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
             },
+            # 2
             {
                 'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
                 'name': 'Синяя куртка The North Face',
                 'price': 23_725,
                 'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
             },
+            # 3
             {
                 'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
                 'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
                 'price': 3390,
                 'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
             },
-
+            # 4
+            {
+                'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png',
+                'name': 'Черный рюкзак Nike Heritage',
+                'price': 2340,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            # 5
+            {
+                'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
+                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+                'price': 13_590,
+                'description': 'Гладкий кожаный верх. Натуральный материал.'
+            },
+            # 6
+            {
+                'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
+                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+                'price': 2890,
+                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.'
+            },
         ]
     }
     return render(request, 'products/products.html', context)
