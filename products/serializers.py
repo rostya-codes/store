8ed199e462d52ddf42e51b05b323cc7eb58e1d80
rostya <rostyamudrik@proton.me>
2024-07-17 19:23:14 +0300

    BasketSerializer and BasketModelViewSet API added

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+from rest_framework import fields
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -13,3 +14,23 @@ class ProductSerializer(serializers.ModelSerializer):
 
         # or    fields = __all__(but not recommendated)
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+class BasketSerializer(serializers.ModelSerializer):
+
+    product = ProductSerializer()  # Передается полная информация про продукт, а не только id
+    sum = fields.FloatField()
+    total_sum = fields.SerializerMethodField()
+    total_quantity = fields.SerializerMethodField()
+
+    class Meta:
+        model = Basket
+        fields = ('id', 'product', 'quantity', 'sum', 'total_sum', 'total_quantity', 'created_timestamp')
+        read_only_fields = ('created_timestamp',)
+
+    def get_total_sum(self, obj):
+        """`get` at the beginning of name is necessarily"""
+        return Basket.objects.filter(user_id=obj.user.id).total_sum()
+
+    def get_total_quantity(self, obj):
+        return Basket.objects.filter(user_id=obj.user.id).total_quantity()
